388144538175c559f66c5f47d4e66104c2e6708a
yincarrey <jean.agurton@utem.cl>
2022-05-02 02:16:58 -0400

    update

--- hola-mundo.c
+++ hola-mundo.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+
+void initialization(int size);
+void *MetodoBurbuja(int *array, int size);
 
 int main(){
     
-    printf("Hola mundo");
+    int size;
 
+    printf("Ingresar largo del array: ");
+    scanf("%d", &size);
+
+    initialization(size);
     return 0;
+};
+
+void initialization(int size){
+
+    int array[size];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < size; i++){
+        array[i] = (10 + (rand() % 91));
+    }
+
+    MetodoBurbuja(array, size);
+    
 }
+
+void *MetodoBurbuja(int *array, int size){
+
+    int aux = 0;
+    
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+
+            if(array[j] > array[j+1]){
+
+                aux = array[j];
+                array[j] = array[j+1];
+                array[j+1] = aux;
+            }
+        }
+    }
+
+    for (int i = 0; i < size; i++){
+        printf("%d\t", array[i]);
+    }
+};
